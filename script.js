5a54fbaefd2444431dca151a24e71b7c28b9ee2b
ismoilnuriddinov <umidafayziyeva89@gmail.com>
2024-08-13 14:55:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let box = 'ismoil'
+// box = 'nuriddinov'
+
+// let ism = 'mars'
+// console.log(box)
+// console.log(ism)
+
+let ism = prompt('ismingizni kiriting')
+let familiya = prompt("familiyangizni kiriting");
+let yoshingiz = prompt("yoshingizni kiriting");
+let tumaningiz = prompt("tumaningizni kiriting");
+
+console.log('ism:' + 'ism')
+console.log("familiya:" + 'familiya');
+console.log("yoshingiz:" + 'yoshingiz');
+console.log("tumaningiz:" + 'tumaningiz');
